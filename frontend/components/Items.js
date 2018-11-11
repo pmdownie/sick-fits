f38e87a241617f7a2e7d6bed279564624093a0f2
Patrick Downie <pmdownie@gmail.com>
2018-11-11 21:49:44 +0100

    Finished 23.

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import Item from "./Item";
 import Pagination from "../components/Pagination";
 import styled from "styled-components";
+import { perPage } from "../config";
 
 const Centered = styled.div`
   text-align: center;
@@ -18,8 +19,8 @@ margin: 0 auto;
 `;
 
 export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -35,8 +36,13 @@ class Items extends Component {
     return (
       <Centered>
         <p>Items</p>
-        <Pagination />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -49,7 +55,7 @@ class Items extends Component {
             );
           }}
         </Query>
-        <Pagination />
+        <Pagination page={this.props.page} />
       </Centered>
     );
   }
--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,11 +1,64 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Link from "next/link";
+import { perPage } from "../config";
 import PaginationStyles from "./styles/PaginationStyles";
 
-const Pagination = () => {
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
+const Pagination = ({ page }) => {
   return (
-    <PaginationStyles>
-      <p>Pagination</p>
-    </PaginationStyles>
+    <Query query={PAGINATION_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error</p>;
+        const count = data.itemsConnection.aggregate.count;
+        const pages = Math.ceil(count / perPage);
+        return (
+          <PaginationStyles>
+            <Link
+              prefetch
+              href={{
+                pathname: "items",
+                query: {
+                  page: page - 1
+                }
+              }}
+            >
+              <a className="prev" aria-disabled={page <= 1}>
+                &#x2190; Prev
+              </a>
+            </Link>
+            <p>
+              Page {page} of {pages}
+            </p>
+            <p>{count} items total</p>
+            <Link
+              prefetch
+              href={{
+                pathname: "items",
+                query: {
+                  page: page + 1
+                }
+              }}
+            >
+              <a className="next" aria-disabled={page >= pages}>
+                Next &#x2192;
+              </a>
+            </Link>
+          </PaginationStyles>
+        );
+      }}
+    </Query>
   );
 };
 
