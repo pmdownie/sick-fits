06ad1f8c775a0d0a7f15e12f8e257ef53360a14c
pmdownie <pmdownie@gmail.com>
2018-11-12 08:57:17 +0100

    Wolring on delete.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,9 +4,9 @@ const Mutations = {
 
     const item = await ctx.db.mutation.createItem(
       {
-        data: { ...args },
+        data: { ...args }
       },
-      info,
+      info
     );
 
     return item;
@@ -21,12 +21,21 @@ const Mutations = {
       {
         data: updates,
         where: {
-          id: args.id,
-        },
+          id: args.id
+        }
       },
-      info,
+      info
     );
   },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import formatMoney from "../lib/formatMoney";
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
+import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
-class Item extends Component {
+export default class Item extends Component {
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  };
+
   render() {
     const { item } = this.props;
     return (
       <ItemStyles>
         {item.image && <img src={item.image} alt={item.title} />}
+
         <Title>
           <Link
             href={{
@@ -24,28 +30,20 @@ class Item extends Component {
         </Title>
         <PriceTag>{formatMoney(item.price)}</PriceTag>
         <p>{item.description}</p>
+
         <div className="buttonList">
           <Link
             href={{
-              pathname: "/update",
+              pathname: "update",
               query: { id: item.id }
             }}
           >
-            <a>Edit</a>
+            <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
   }
 }
-
-Item.propTypes = {
-  item: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired
-  })
-};
-
-export default Item;
